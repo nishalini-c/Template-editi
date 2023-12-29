3593a6a0c6a47db5ad83e0d3282e23d24354afc1
Nishalini-c <shaluisha29@gmail.com>
2023-12-29 11:06:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Header from './header';
+import Footer from './footer';
+import Content from './content';
+
+function App() {
+  return (
+    <div className="App">
+    <Header></Header>
+    <Content></Content>
+    <Footer></Footer>
+    </div>
+  );
+}
+
+export default App ;
